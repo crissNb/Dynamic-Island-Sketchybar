8084bc6818b818b98a8db2c01eabe9f0d1b6cab1
crissnb <crissnb472@gmail.com>
2022-09-29 11:17:25 +0200

    remove

--- dynamic_island_icons.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-VOLUME_MAX=􀊩
-VOLUME_MED=􀊧
-VOLUME_LOW=􀊥
-VOLUME_MUTED=􀊡
--- plugins/dynamic_island/dynamic_island.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Notifications
-# bash "$HOME/.config/sketchybar/plugins/dynamic_island/islands/notification/handler.sh"
-
-# VOLUME
-bash "$HOME/.config/sketchybar/plugins/dynamic_island/islands/volume/handler.sh"
