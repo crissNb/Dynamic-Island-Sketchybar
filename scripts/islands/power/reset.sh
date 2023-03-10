c8a17d9fe4ff0689218fdb9b4ee849d79b08eaf0
Austin Horstman <khaneliman12@gmail.com>
2023-03-09 21:13:13 -0600

    lint fixes power

--- scripts/islands/power/reset.sh
+++ scripts/islands/power/reset.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 sketchybar --set island.power_text drawing=off \
-		   --set island.power_icon drawing=off \
-		   --set island.power_background drawing=off \
-		   --set island background.drawing=true
+	--set island.power_icon drawing=off \
+	--set island.power_background drawing=off \
+	--set island background.drawing=true
 
 sleep 0.1
 
