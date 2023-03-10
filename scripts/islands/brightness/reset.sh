1ab090c3d1d62f1fe9f3de53e610948a3d89c4d0
Austin Horstman <khaneliman12@gmail.com>
2023-03-09 20:42:49 -0600

    lint fixes brightness

--- scripts/islands/brightness/reset.sh
+++ scripts/islands/brightness/reset.sh
@@ -3,12 +3,12 @@
 sketchybar --trigger dynamic_island_request
 
 sketchybar --set island.brightness_placeholder1 drawing=off \
-		   --set island.brightness_placeholder2 drawing=off \
-		   --set island.brightness_placeholder3 drawing=off \
-		   --set island.brightness_icon drawing=off \
-		   --set island.brightness_bar drawing=off \
-		   --set island		background.drawing=true \
-		   					popup.horizontal=on
+	--set island.brightness_placeholder2 drawing=off \
+	--set island.brightness_placeholder3 drawing=off \
+	--set island.brightness_icon drawing=off \
+	--set island.brightness_bar drawing=off \
+	--set island background.drawing=true \
+	popup.horizontal=on
 
 sleep 0.1
 
